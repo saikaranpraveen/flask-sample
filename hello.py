9e4b5f36536c1a54181dcfee2acb1e32c725d16e
SAIKARAN PRAVEEN <84612613+saikaranpraveen@users.noreply.github.com>
2022-11-20 19:28:44 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_world():
-    return "<h1>Hello world from Kubernetes - test 16!</h1>"
+    return "<h1>Hello world from Kubernetes - test 17!</h1>"
 
 
 if __name__ == '__main__':
